b5eb2753b7dec3698cc411b2d5dd10b92a304782
Vinicius Vasconcelos <100451700+viny1290@users.noreply.github.com>
2023-03-18 22:24:34 -0300

    SistemaSolar

---
+++ src/componentes/Sol/Sol.jsx
@@ -0,0 +1,13 @@
+import Sun from './sol.png';
+
+export default function Sol() {
+
+    return(
+
+        <div className="sun">
+            <img className='sun-img' src={Sun} alt="Sol" />
+        </div>
+
+    )
+
+}
---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,15 @@
+import Planetas from "../../componentes/Planetas";
+import Sol from "../../componentes/Sol/Sol";
+
+export default function PaginaInicial() {
+
+    return(
+
+        <>
+            <Sol />
+            <Planetas />
+        </>
+
+    )
+
+}
